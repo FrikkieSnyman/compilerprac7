b8d2d31bd579bfcc2d71b862fabf455b2bc6816d
Frikkie Snyman <frikkie.snyman@live.co.za>
2015-05-05 20:45:39 +0200

    Completed taskC

--- taskC/Kill.java
+++ taskC/Kill.java
@@ -15,6 +15,27 @@ public class Kill{
 			e.printStackTrace();
 		}
 
+		for (int i = 0; i < lines.size(); ++i){
+			String analyse = lines.get(i);
+			LinkedList<String> tmp = kill.get(i+1);
+			if (tmp == null){
+				tmp = new LinkedList<String>();
+			}
+
+			if (analyse.contains("=")){
+				if (!analyse.contains("IF")){
+					String[] split = analyse.split("=");		// index 0 = left of '=', 1 = right of '='
+					for (int k = 0; k < split.length; ++k){
+						split[k] = split[k].trim();
+					}
+					String[] left = split[0].split(" ");
+					if (Character.isLetter(left[1].charAt(0))){	// variable name
+						tmp.add(left[1]);
+					}
+				}
+			}
+			kill.put(i+1,tmp);
+		}
 		System.out.println(kill.toString());
 	}
 }
